bfbf6994568167c1fbb32179df44f95fcc54aac7
RemiErr <Re.Err236@gmail.com>
2024-06-11 17:13:56 +0800

    整理合併變更

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,15 +9,6 @@ class Product extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
-    // 要確保這邊的元素應該和 migration 裡的一樣
-    protected $fillable = [
-        'name',
-        'qty',
-        'price',
-        'description'
-    ];
-=======
     protected $table = 'products';
     protected $primaryKey = 'product_id';
     public $timestamps = true;
@@ -33,5 +24,4 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class, 'order_items', 'product_id', 'order_id');
     }
->>>>>>> dev-240517
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,6 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
-=======
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -73,5 +31,4 @@ class User extends Model
         // Order 的 user_id = this.user_id
         return $this->hasMany(Order::class, 'user_id', 'user_id');
     }
->>>>>>> dev-240517
 }
