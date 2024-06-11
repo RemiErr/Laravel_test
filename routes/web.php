b90100b5f127da16718a5638175568ccac044528
RemiErr <Re.Err236@gmail.com>
2024-06-11 17:16:16 +0800

    整理合併變更

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
-<<<<<<< HEAD
-/*
-    Route 負責依照請求，將參數帶到不同頁面
-*/
-
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-=======
 
 use Illuminate\Support\Facades\Route;
 
->>>>>>> dev-240517
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,20 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-
-// [] 陣列第二元素 = Controller 裡的 Function 名稱
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-
-Route::post('/product', [ProductController::class, 'store'])->name('product.store');
-
-// 更新資料時，要用 put
-Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
-
-Route::delete('/product/{product}/destroy', [ProductController::class, 'destroy'])->name('product.destroy');
-
-=======
->>>>>>> dev-240517
