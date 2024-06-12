94214db8307b27de77e574961f818145c232bdba
RemiErr <Re.Err236@gmail.com>
2024-06-12 11:41:27 +0800

    model - pivot

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -25,6 +25,7 @@ class Order extends Model
     public function products()
     {
         // order_items 表裡 [this.order_id, Product.product_id]
-        return $this->belongsToMany(Product::class, 'order_items', 'order_id', 'product_id');
+        // pivot 代表中介表模型，預設只提供 Key ，多出來的欄位用 withPivot 設定
+        return $this->belongsToMany(Product::class, 'order_items', 'order_id', 'product_id')->withPivot('item_quantity');
     }
 }
