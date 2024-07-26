555217782ab826789c2120d1c0291246c10f82a5
RemiErr <Re.Err236@gmail.com>
2024-07-26 13:06:08 +0800

    新增資料驗證，並調整回傳的 status

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class AuthController extends Controller
 {
@@ -16,17 +18,37 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        // 驗證資料格式
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'user_name' => 'required|string',
+                'password' => 'required|string',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                // 請求語法的格式錯誤 (400 Bad Request)
+                'status' => 400,
+                'message' => 'Parameters Error',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        // 比對資料內容
         $credentials = request(['user_name', 'password']);
 
         if (!$token = auth()->attempt($credentials)) {
             return response()->json([
-                'status' => 'fail',
-                'message' => 'invalid credentials'
+                // 請求未完成 (401 Bad Request)
+                'status' => 401,
+                'message' => 'Invalid Credentials'
             ], 401);
         }
 
         return response()->json([
-            'status' => 'success',
+            'status' => 200,
             'token_type' => 'Bearer',
             'token' => $token,
         ]);
@@ -35,25 +57,36 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->logout();
-        return response()->json(['status' => 'success']);
+        return response()->json(['status' => 200]);
     }
 
     public function register(Request $request)
     {
-        // $request->validate([
-        //     'user_name' => 'required|string',
-        //     'user_address' => 'required|string',
-        //     'user_phone' => 'required|string',
-        //     'user_email' => 'required|email',
-        //     'password' => 'required|string',
-        // ]);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'user_name' => 'required|string',
+                'user_address' => 'required|string',
+                'user_phone' => 'required|string',
+                'user_email' => 'required|email',
+                'password' => 'required|string',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'message' => 'Parameters Error',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
 
         // 建立新使用者
         $user = User::create([
-            'user_name' => $request->name,
-            'user_address' => $request->address,
-            'user_phone' => $request->phone,
-            'user_email' => $request->email,
+            'user_name' => $request->user_name,  
+            'user_address' => $request->user_address,
+            'user_phone' => $request->user_phone,
+            'user_email' => $request->user_email,
             'user_password' => bcrypt($request->password),
         ]);
 
@@ -61,7 +94,7 @@ class AuthController extends Controller
         $user->save();
 
         return response()->json([
-            'status' => 'success',
+            'status' => 200,
             'token_type' => 'Bearer',
             'token' => $token,
         ]);
@@ -71,14 +104,15 @@ class AuthController extends Controller
     {
         $token = $request->token;
         $user = JWTAuth::setToken($token)->toUser();
+        
         if (count((array)$user) > 0) {
             return response()->json([
-                'status' => 'success',
+                'status' => 200,
                 'user' => $user
             ]);
         } else {
             return response()->json([
-                'status' => 'fail'
+                'status' => 401
             ], 401);
         }
     }
